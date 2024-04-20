fd5b39c7826e493851cf8bf281ee474e9ecbbc11
Wyatt Johnson <accounts+github@wyattjoh.ca>
2024-04-20 01:28:50 -0600

    feat: replace createNextDescribe with nextTestSetup

--- test/production/app-dir/barrel-optimization/basic/index.test.ts
+++ test/production/app-dir/barrel-optimization/basic/index.test.ts
@@ -1,17 +1,15 @@
-import { createNextDescribe } from 'e2e-utils'
+import { nextTestSetup } from 'e2e-utils'
 
 describe('Skipped in Turbopack', () => {
-  createNextDescribe(
-    'app-dir - optimizePackageImports - basic',
-    {
+  describe('app-dir - optimizePackageImports - basic', () => {
+    const { next } = nextTestSetup({
       files: __dirname,
-    },
-    ({ next }) => {
-      it('should build successfully', async () => {
-        // Ensure that MUI is working
-        const $ = await next.render$('/')
-        expect(await $('#client-mod').text()).toContain('client:default')
-      })
-    }
-  )
+    })
+
+    it('should build successfully', async () => {
+      // Ensure that MUI is working
+      const $ = await next.render$('/')
+      expect(await $('#client-mod').text()).toContain('client:default')
+    })
+  })
 })
